f17eab34c6878c47ad7159c6c730e48b33f208f8
ajido <ajido@me.com>
2016-09-22 20:49:07 +0900

    優待存在有無を削除 #6

--- scrape.js
+++ scrape.js
@@ -133,7 +133,6 @@ const download = (portfolio, summary) => {
       '名目配当優待利回り',
       '保有株配当金額',
       '保有株優待額',
-      '優待存在有無',
       'PER',
       'PBR',
       '予想配当',
@@ -209,7 +208,6 @@ const download = (portfolio, summary) => {
         data['名目配当優待利回り'],
         data['保有株配当金額'],
         data['保有株優待額'],
-        data['優待存在有無'],
         data['PER'],
         data['PBR'],
         data['予想配当'],
@@ -418,157 +416,146 @@ const getDetails = (index) => {
       }
     });
 
-    const giftUrl = [].filter.call(container.querySelectorAll('[name="FormKabuka"] [class*="tab"] a'), (v) => {
-      return /株主優待/.test(v.textContent);
+    const repUrl = [].filter.call(container.querySelectorAll('[name="FormKabuka"] [class*="tab"] a'), (v) => {
+      return /四季報/.test(v.textContent);
     })[0].getAttribute('href');
 
-    // 株主優待ページに移動
-    request(giftUrl, (giftContainer) => {
-      [].forEach.call(giftContainer.querySelectorAll('.tbl690 tr th'), (v) => {
-        if (/^優待利回り$/.test(v.textContent)) data['優待存在有無'] = v.nextElementSibling.textContent.trim();
-      });
+    data['四季報'] = { '四季報リンク': document.location.origin + repUrl };
 
-      const repUrl = [].filter.call(container.querySelectorAll('[name="FormKabuka"] [class*="tab"] a'), (v) => {
-        return /四季報/.test(v.textContent);
-      })[0].getAttribute('href');
+    // 四季報ページに移動
+    request(repUrl, (repContainer) => {
+      const keys = [
+        'ＵＲＬ', '決算', '設立', '上場', '特色', '連結事業', '業種', '仕入先',
+        '販売先', '比較会社', '本社', '支社', '従業員', '証券', '銀行', '連結', '会社業績修正'
+      ];
 
-      data['四季報'] = { '四季報リンク': document.location.origin + repUrl };
+      if (repContainer.querySelectorAll('.shikihouBox01 table')[0]) {
+        repContainer.querySelectorAll('.shikihouBox01 table')[0].textContent.split(/[\r\n]/).forEach((v) => {
+          if (/【([^】]+)】(.*?)$/.test(v)) {
+            if (keys.indexOf(RegExp.$1) !== -1) {
+              data['四季報'][RegExp.$1] = RegExp.$2;
+            } else if (RegExp.$1 === '四半期進捗率' && !data['四季報']['会社業績修正']) {
+              data['四季報']['会社業績修正'] = RegExp.$2;
+            } else {
+              if (!data['四季報']['コメント１']) {
+                data['四季報']['コメント１'] = '【' + RegExp.$1 + '】 ' + RegExp.$2;
+              } else if (!data['四季報']['コメント２']) {
+                data['四季報']['コメント２'] = '【' + RegExp.$1 + '】 ' + RegExp.$2;
+              }
+            }
+          }
+        });
 
-      // 四季報ページに移動
-      request(repUrl, (repContainer) => {
+        const stocks = repContainer.querySelectorAll('.shikihouBox01 > table table:nth-child(2) tr > td:nth-child(3)');
+        data['四季報']['浮動株'] = Number(stocks[0].textContent.trim().replace('%', ''));
+        data['四季報']['特定株'] = Number(stocks[1].textContent.trim().replace('%', ''));
+      }
+
+      let finRepUrl;
+
+      if (repContainer.querySelectorAll('.shikihouBox01 table')[0]) {
+        finRepUrl = [].filter.call(repContainer.querySelectorAll('[class*="tab"] a'), (v) => {
+          return /財務状況/.test(v.textContent);
+        })[0].getAttribute('href');
+      } else {
+        finRepUrl = repUrl;
+      }
+
+      data['財務状況'] = {};
+
+      // 財務状況ページに移動
+      request(finRepUrl, (finRepContainer) => {
         const keys = [
-          'ＵＲＬ', '決算', '設立', '上場', '特色', '連結事業', '業種', '仕入先',
-          '販売先', '比較会社', '本社', '支社', '従業員', '証券', '銀行', '連結', '会社業績修正'
+          '時価総額', '営業CF', '投資CF', '財務CF', '現金等',
+          'ROE', 'ROA', '総資産', '自己資本比率', '有利子負債'
         ];
 
-        if (repContainer.querySelectorAll('.shikihouBox01 table')[0]) {
-          repContainer.querySelectorAll('.shikihouBox01 table')[0].textContent.split(/[\r\n]/).forEach((v) => {
-            if (/【([^】]+)】(.*?)$/.test(v)) {
-              if (keys.indexOf(RegExp.$1) !== -1) {
-                data['四季報'][RegExp.$1] = RegExp.$2;
-              } else if (RegExp.$1 === '四半期進捗率' && !data['四季報']['会社業績修正']) {
-                data['四季報']['会社業績修正'] = RegExp.$2;
-              } else {
-                if (!data['四季報']['コメント１']) {
-                  data['四季報']['コメント１'] = '【' + RegExp.$1 + '】 ' + RegExp.$2;
-                } else if (!data['四季報']['コメント２']) {
-                  data['四季報']['コメント２'] = '【' + RegExp.$1 + '】 ' + RegExp.$2;
-                }
-              }
+        [].forEach.call(finRepContainer.querySelectorAll('.shikihouBox01 > table:nth-child(3) td.mtext:not([align="right"])'), (v) => {
+          const key = v.textContent.trim();
+
+          if (keys.indexOf(key) !== -1) {
+            if (key === '時価総額') {
+              data['財務状況'][key] = v.nextElementSibling.textContent.trim().replace(/\[[0-9]+\]$/, '');
+            } else {
+              data['財務状況'][key] = Number(v.nextElementSibling.textContent.trim().replace(/[%,]/g, ''));
             }
-          });
-
-          const stocks = repContainer.querySelectorAll('.shikihouBox01 > table table:nth-child(2) tr > td:nth-child(3)');
-          data['四季報']['浮動株'] = Number(stocks[0].textContent.trim().replace('%', ''));
-          data['四季報']['特定株'] = Number(stocks[1].textContent.trim().replace('%', ''));
-        }
-
-        let finRepUrl;
-
-        if (repContainer.querySelectorAll('.shikihouBox01 table')[0]) {
-          finRepUrl = [].filter.call(repContainer.querySelectorAll('[class*="tab"] a'), (v) => {
-            return /財務状況/.test(v.textContent);
-          })[0].getAttribute('href');
-        } else {
-          finRepUrl = repUrl;
-        }
-
-        data['財務状況'] = {};
-
-        // 財務状況ページに移動
-        request(finRepUrl, (finRepContainer) => {
-          const keys = [
-            '時価総額', '営業CF', '投資CF', '財務CF', '現金等',
-            'ROE', 'ROA', '総資産', '自己資本比率', '有利子負債'
-          ];
-
-          [].forEach.call(finRepContainer.querySelectorAll('.shikihouBox01 > table:nth-child(3) td.mtext:not([align="right"])'), (v) => {
-            const key = v.textContent.trim();
-
-            if (keys.indexOf(key) !== -1) {
-              if (key === '時価総額') {
-                data['財務状況'][key] = v.nextElementSibling.textContent.trim().replace(/\[[0-9]+\]$/, '');
-              } else {
-                data['財務状況'][key] = Number(v.nextElementSibling.textContent.trim().replace(/[%,]/g, ''));
-              }
-            }
-
-            if (key === 'ROA' || key === 'ROE') {
-              data['財務状況']['予' + key] = Number(v.nextElementSibling.nextElementSibling.textContent.trim().replace(/[予%]/g, ''));
-            }
-          });
-
-          let cur, prev;
-
-          [].forEach.call(finRepContainer.querySelectorAll('.shikihouBox01 > table:nth-child(2) table td.mtext[align="left"]'), (v) => {
-            const key = v.textContent.trim();
-            if (/^(連|単|◇|◎)/.test(key) === false) return;
-
-            if (/予/.test(key) && !cur) cur = v;
-            if (!cur) prev = v;
-          });
-
-          data['財務状況']['昨年度'] = {};
-          data['財務状況']['本年度'] = {};
-
-          if (cur && prev) {
-            data['財務状況']['昨年度'] = {
-              '売上': Number(prev.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '営業利益': Number(prev.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '経常利益': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '純利益': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '一株利益': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '一株配当': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '').replace(/[特記]/g, '').split('～').pop())
-            };
-
-            data['財務状況']['本年度'] = {
-              '予想売上': Number(cur.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '予想営業利益': Number(cur.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '予想経常利益': Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '予想純利益': Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '予想一株利益':  Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
-              '予想一株配当':  Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '').replace(/[特記]/g, '').split('～').pop())
-            };
           }
 
-          const analyzeUrl = [].filter.call(container.querySelectorAll('[name="FormKabuka"] [class*="tab"] a'), (v) => {
-            return /分析/.test(v.textContent);
-          })[0].getAttribute('href');
+          if (key === 'ROA' || key === 'ROE') {
+            data['財務状況']['予' + key] = Number(v.nextElementSibling.nextElementSibling.textContent.trim().replace(/[予%]/g, ''));
+          }
+        });
 
-          data['分析'] = {};
+        let cur, prev;
 
-          // 分析ページに移動
-          request(analyzeUrl, (analyzeContainer) => {
-            const formDataOrigin = new FormData(analyzeContainer.querySelectorAll('form[name="formSwitch"]')[0]);
-            const formData = new URLSearchParams();
+        [].forEach.call(finRepContainer.querySelectorAll('.shikihouBox01 > table:nth-child(2) table td.mtext[align="left"]'), (v) => {
+          const key = v.textContent.trim();
+          if (/^(連|単|◇|◎)/.test(key) === false) return;
 
-            for (const key of formDataOrigin.keys()) {
-              formData.append(key, formDataOrigin.get(key));
+          if (/予/.test(key) && !cur) cur = v;
+          if (!cur) prev = v;
+        });
+
+        data['財務状況']['昨年度'] = {};
+        data['財務状況']['本年度'] = {};
+
+        if (cur && prev) {
+          data['財務状況']['昨年度'] = {
+            '売上': Number(prev.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '営業利益': Number(prev.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '経常利益': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '純利益': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '一株利益': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '一株配当': Number(prev.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '').replace(/[特記]/g, '').split('～').pop())
+          };
+
+          data['財務状況']['本年度'] = {
+            '予想売上': Number(cur.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '予想営業利益': Number(cur.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '予想経常利益': Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '予想純利益': Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '予想一株利益':  Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '')),
+            '予想一株配当':  Number(cur.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim().replace(/,/g, '').replace(/[特記]/g, '').split('～').pop())
+          };
+        }
+
+        const analyzeUrl = [].filter.call(container.querySelectorAll('[name="FormKabuka"] [class*="tab"] a'), (v) => {
+          return /分析/.test(v.textContent);
+        })[0].getAttribute('href');
+
+        data['分析'] = {};
+
+        // 分析ページに移動
+        request(analyzeUrl, (analyzeContainer) => {
+          const formDataOrigin = new FormData(analyzeContainer.querySelectorAll('form[name="formSwitch"]')[0]);
+          const formData = new URLSearchParams();
+
+          for (const key of formDataOrigin.keys()) {
+            formData.append(key, formDataOrigin.get(key));
+          }
+
+          const xhr = new XMLHttpRequest();
+
+          xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+              const container = document.implementation.createHTMLDocument().documentElement;
+              container.innerHTML = xhr.responseText;
+
+              const keys = ['成長性', '割安性', '企業規模', 'テクニカル', '財務健全性', '市場トレンド'];
+              [].forEach.call(container.querySelectorAll('table[summary="ランク"] tr > th'), (v) => {
+                if (keys.indexOf(v.textContent.trim()) !== -1) {
+                  data['分析'][v.textContent.trim()] = Number(v.nextElementSibling.nextElementSibling.textContent.trim());
+                }
+              });
+
+              console.log(data);
+              getDetails(index + 1);
             }
+          }
 
-            const xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = () => {
-              if (xhr.readyState === 4) {
-                const container = document.implementation.createHTMLDocument().documentElement;
-                container.innerHTML = xhr.responseText;
-
-                const keys = ['成長性', '割安性', '企業規模', 'テクニカル', '財務健全性', '市場トレンド'];
-                [].forEach.call(container.querySelectorAll('table[summary="ランク"] tr > th'), (v) => {
-                  if (keys.indexOf(v.textContent.trim()) !== -1) {
-                    data['分析'][v.textContent.trim()] = Number(v.nextElementSibling.nextElementSibling.textContent.trim());
-                  }
-                });
-
-                console.log(data);
-                getDetails(index + 1);
-              }
-            }
-
-            xhr.open('POST', 'https://www.kabumap.com/servlets/Query');
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send(formData.toString().replace(/%20/g, '+'));
-          });
+          xhr.open('POST', 'https://www.kabumap.com/servlets/Query');
+          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+          xhr.send(formData.toString().replace(/%20/g, '+'));
         });
       });
     });
